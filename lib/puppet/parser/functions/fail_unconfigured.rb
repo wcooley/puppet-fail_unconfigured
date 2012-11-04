54751d4df5efcf9c1d06b2e52265638eb2e624c7
Wil Cooley <wcooley@nakedape.cc>
2012-11-04 12:06:06 -0800

    Handle unset or unknown variables better
    * module, class and node can now all be unset without error
    
    * Any arguments that cannot be resolved are excluded from the resulting
      message.

--- lib/puppet/parser/functions/fail_unconfigured.rb
+++ lib/puppet/parser/functions/fail_unconfigured.rb
@@ -1,23 +1,20 @@
 module Puppet::Parser::Functions
   newfunction(:fail_unconfigured, :doc => "Fail with a canned 'not_configured' message."
              ) do |args|
-    node = lookupvar('hostname')
-    modulename = self.source.module_name || 'unknown'
-    puppet_class = self.source.name || 'unknown'
 
     if args.length == 0
         args = ['operatingsystem', 'operatingsystemrelease', 'osfamily' ]
     end
+    args = ['node'] + args
 
-    argpairs = [
-        "error=not_configured",
-        "node=#{node}",
-        "module=#{modulename}",
-        "class=#{puppet_class}"
-    ]
+    argpairs = [ "error=not_configured" ]
+    argpairs << "module=#{self.source.module_name}" if self.source.module_name
+    argpairs << "class=#{self.source.name}"         if self.source.name
 
     args.each do |arg|
-        val = lookupvar(arg).downcase || 'unknown'
+        val = lookupvar(arg) || :undefined
+        next if val == :undefined
+        val.downcase!
         argpairs.push("#{arg}=#{val}")
     end
 
